3a2234b7426d98c052c5bf2bcc45dd5f8216cce3
Emmanouil Athanasopoulos <manos.athanasopoulos@yahoo.gr>
2023-01-03 22:06:31 +0200

    refactored util functions

--- src/main/java/com/codehub/techniconrenovations/util/InputHandler.java
+++ src/main/java/com/codehub/techniconrenovations/util/InputHandler.java
@@ -2,24 +2,8 @@ package com.codehub.techniconrenovations.util;
 
 import com.codehub.techniconrenovations.enums.PropertyType;
 import com.codehub.techniconrenovations.enums.RepairType;
-import java.time.Year;
 
 public class InputHandler {
-
-    public String e9(String e9) {
-        return e9.replaceAll("[^a-zA-Z0-9\\s]", "").trim();
-    }
-
-    public String address(String message) {
-        return message.replaceAll("[^a-zA-Z0-9\\s]", "").trim();
-    }
-
-    public int constructionYear(int year) {
-        if (year < 1800 || year > Year.now().getValue()) {
-            return -1;
-        }
-        return year;
-    }
 
     public PropertyType selectPropertyType(String input) {
         return switch (input) {
@@ -40,27 +24,12 @@ public class InputHandler {
             default -> null;
         };
     }
-    
-    public String description(String message) {
-        return message.replaceAll("[^a-zA-Z0-9\\s]", "");
-    }
-    
-    public String names(String message) {
-        return message.replaceAll("[^a-zA-Z0-9\\s]", "").trim();
-    }
-    
+
     public String email(String email) {
         if (!email.contains("@") || !email.contains(".")) {
             return null;
         }
         return email;
-    }
-    
-    public String password(String password) {
-        if (password.contains(" ") || password.length() < 6) {
-            return null;
-        }
-        return password;
     }
     
     public String phoneNumber(String phoneNumber) {
--- src/main/java/com/codehub/techniconrenovations/util/UtilFunctions.java
+++ src/main/java/com/codehub/techniconrenovations/util/UtilFunctions.java
@@ -1,15 +1,16 @@
 package com.codehub.techniconrenovations.util;
 
-import com.codehub.techniconrenovations.services.impl.AdminServicesImpl;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import com.codehub.techniconrenovations.repository.impl.PropertyRepairRepositoryImpl;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Date;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UtilFunctions {
+    
+    private static final Logger logger = LoggerFactory.getLogger(PropertyRepairRepositoryImpl.class);
 
     /**
      * Converts a String to date
@@ -17,46 +18,28 @@ public class UtilFunctions {
      * @param date
      * @return Date
      */
-    public static Date convertToDate(String date) {       
+    public static Date convertToDate(String date) {        
         if (!date.equals("null")) {
-            String[] dateSplit =date.split("/");
+            String[] dateSplit = date.split("/");
             return new Date(Integer.parseInt(dateSplit[2]) - 1900, Integer.parseInt(dateSplit[1]) - 1, Integer.parseInt(dateSplit[0]));
         }
-        return new Date(1,0,1);//null date flag
+        return new Date(1, 0, 1);//null date flag
     }
     
-    /**
-     * Checks a date if it is equal to null date flag.
-     * @param date
-     * @return bool
-     */
-    public static boolean CheckDate(Date date){
-        return date.compareTo(new Date(1,0,1)) == 0;
+    public static String get_SHA_512_SecurePassword(String passwordToHash, String salt) {
+        String generatedPassword = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            md.update(salt.getBytes(StandardCharsets.UTF_8));
+            byte[] bytes = md.digest(passwordToHash.getBytes(StandardCharsets.UTF_8));
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bytes.length; i++) {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            generatedPassword = sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            logger.error("" + e);
+        }
+        return generatedPassword;
     }
-    
-    /**
-     * Prints a list of objects.
-     * @param list
-     */
-    public static void printList(List<? extends Object> list){
-        if(list.isEmpty())System.err.println("No results matching your criteria found!");
-        list.forEach(o -> System.out.println(o.toString()));
-    }
-    
-    /**
-     * Saves a list of objects to a file.
-     * @param list
-     * @param classname
-     */
-    public static void reportsToFile(List<? extends Object> list,String classname) {
-        File file = new File(classname+".csv");
-        System.out.println("Generating file for Repairs");
-        try ( PrintWriter pw = new PrintWriter(file)) {
-            list.forEach(o ->pw.println(o));
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(AdminServicesImpl.class.getName()).log(Level.SEVERE, null, ex);
-        }  
-    }
-    
 }
-
