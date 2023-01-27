380cd38952e984d7fe0e2d4cc8982336247e3bd7
Emmanouil Athanasopoulos <manos.athanasopoulos@yahoo.gr>
2023-01-27 15:57:29 +0200

    data initialization grouped

--- src/test/java/com/codehub/techniconrenovations/resources/AdminResourceTest.java
+++ src/test/java/com/codehub/techniconrenovations/resources/AdminResourceTest.java
@@ -4,7 +4,14 @@ import com.codehub.techniconrenovations.dto.PropertyDto;
 import com.codehub.techniconrenovations.dto.RepairDto;
 import com.codehub.techniconrenovations.dto.RestApiResult;
 import com.codehub.techniconrenovations.dto.UserDto;
+import com.codehub.techniconrenovations.enums.PropertyType;
+import com.codehub.techniconrenovations.enums.RepairType;
+import com.codehub.techniconrenovations.enums.Status;
+import com.codehub.techniconrenovations.model.Property;
+import com.codehub.techniconrenovations.model.PropertyOwner;
+import com.codehub.techniconrenovations.model.PropertyRepair;
 import com.codehub.techniconrenovations.services.AdminServices;
+import com.codehub.techniconrenovations.util.UtilFunctions;
 import jakarta.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,24 +25,33 @@ public class AdminResourceTest {
     
     AdminResource adminResource;
     AdminServices adminServices;
-    List<RepairDto> repairs;
-    RepairDto repair;
+    Property property;
+    PropertyDto propertyDto;
+    PropertyOwner propertyOwner;
+    UserDto userDto;
+    PropertyRepair repair;
+    RepairDto repairDto;
     List<PropertyDto> properties;
-    PropertyDto property;
-    List<UserDto> owners;
-    UserDto owner; 
+    List<RepairDto> repairs;
+    List<UserDto> users;
     
     @Before
     public void setUp() {
         adminResource = new AdminResource();
         adminServices = mock(AdminServices.class);
         adminResource.adminServices = adminServices;
-        repairs = new ArrayList<>();
-        repair = new RepairDto();
+        propertyOwner = new PropertyOwner(1, "name", "surname", "address", "phoneNumber", "email", "username", "password", "typeOfUser");
+        userDto = new UserDto(propertyOwner);
+        users = new ArrayList<>();
+        users.add(userDto);
+        property = new Property("E9", "123 Main St", 2000, PropertyType.APARTMENT_BUILDING, propertyOwner);
+        propertyDto = new PropertyDto(property);
+        repair = new PropertyRepair(propertyOwner, property, RepairType.PAINTING, UtilFunctions.stringToDate("00/00/0000"), "shortDescription", "description", Status.PENDING);
+        repairDto = new RepairDto(repair);
         properties = new ArrayList<>();
-        property = new PropertyDto();
-        owners = new ArrayList<>();
-        owner = new UserDto(); 
+        properties.add(propertyDto);
+        repairs = new ArrayList<>();
+        repairs.add(repairDto);
     }
 
     @Test
@@ -47,11 +63,6 @@ public class AdminResourceTest {
 
     @Test
     public void testGetPendingRepairs() {
-        repair.setRepairId(1);
-        repair.setProposedCost("1000.0");
-        repair.setActualStartDate("01/01/2022");
-        repair.setActualEndDate("01/10/2022");
-        repairs.add(repair);
         when(adminServices.getPendingRepairs()).thenReturn(repairs);
         RestApiResult result = adminResource.getPendingRepairs();
         assertEquals(200, result.getErrorCode());
@@ -61,7 +72,7 @@ public class AdminResourceTest {
 
     @Test
     public void testGetPendingRepairsWithNoResults() {
-        when(adminServices.getPendingRepairs()).thenReturn(repairs);
+        when(adminServices.getPendingRepairs()).thenReturn(new ArrayList<>());
         RestApiResult result = adminResource.getPendingRepairs();
         assertEquals(404, result.getErrorCode());
         assertEquals("UnSuccessful", result.getDescription());
@@ -70,27 +81,21 @@ public class AdminResourceTest {
 
     @Test
     public void testProposeCost() {
-        Response response = adminResource.proposeCost(repair);
+        Response response = adminResource.proposeCost(repairDto);
         assertEquals(200, response.getStatus());
         assertEquals("Successful!", response.getEntity());
     }
 
     @Test
     public void testProposeStartEndDates() {
-        Response response = adminResource.proposeStartEndDates(repair);
+        Response response = adminResource.proposeStartEndDates(repairDto);
         assertEquals(200, response.getStatus());
         assertEquals("Successful!", response.getEntity());
     }
 
     @Test
     public void testGetAllRepairs() {
-        repair.setRepairId(1);
-        repair.setProposedCost("1000.0");
-        repair.setActualStartDate("01/01/2022");
-        repair.setActualEndDate("01/10/2022");
-        repairs.add(repair);
         when(adminServices.getAllRepairs()).thenReturn(repairs);
-
         RestApiResult result = adminResource.getAllRepairs();
         assertEquals(200, result.getErrorCode());
         assertEquals("Successful", result.getDescription());
@@ -99,7 +104,7 @@ public class AdminResourceTest {
 
     @Test
     public void testGetAllRepairsWithNoResults() {
-        when(adminServices.getAllRepairs()).thenReturn(repairs);
+        when(adminServices.getAllRepairs()).thenReturn(new ArrayList<>());
         RestApiResult result = adminResource.getAllRepairs();
         assertEquals(404, result.getErrorCode());
         assertEquals("UnSuccessful", result.getDescription());
@@ -110,7 +115,7 @@ public class AdminResourceTest {
     public void testGetProperties() {
         property.setPropertyId("123");
         property.setPropertyAddress("123 Main St");
-        properties.add(property);
+        properties.add(propertyDto);
         when(adminServices.getProperties()).thenReturn(properties);
         RestApiResult result = adminResource.getProperties();
         assertEquals(200, result.getErrorCode());
@@ -120,7 +125,7 @@ public class AdminResourceTest {
 
     @Test
     public void testGetPropertiesWithNoResults() {
-        when(adminServices.getProperties()).thenReturn(properties);
+        when(adminServices.getProperties()).thenReturn(new ArrayList<>());
         RestApiResult result = adminResource.getProperties();
         assertEquals(404, result.getErrorCode());
         assertEquals("UnSuccessful", result.getDescription());
@@ -129,21 +134,16 @@ public class AdminResourceTest {
 
     @Test
     public void testGetOwners() {
-        owner.setVatNumber(1);
-        owner.setName("John");
-        owner.setSurname("Doe");
-        owners.add(owner);
-        when(adminServices.getOwners()).thenReturn(owners);
-
+        when(adminServices.getOwners()).thenReturn(users);
         RestApiResult result = adminResource.getOwners();
         assertEquals(200, result.getErrorCode());
         assertEquals("Successful", result.getDescription());
-        assertEquals(owners, result.getData());
+        assertEquals(users, result.getData());
     }
 
     @Test
     public void testGetOwnersWithNoResults() {
-        when(adminServices.getOwners()).thenReturn(owners);
+        when(adminServices.getOwners()).thenReturn(new ArrayList<>());
         RestApiResult result = adminResource.getOwners();
         assertEquals(404, result.getErrorCode());
         assertEquals("UnSuccessful", result.getDescription());
