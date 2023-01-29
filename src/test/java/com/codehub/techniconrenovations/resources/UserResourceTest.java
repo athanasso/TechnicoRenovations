acf1d21e5d34b51699a41c8489018641e06d059c
Emmanouil Athanasopoulos <manos.athanasopoulos@yahoo.gr>
2023-01-29 19:19:37 +0200

    Update UserResourceTest.java

--- src/test/java/com/codehub/techniconrenovations/resources/UserResourceTest.java
+++ src/test/java/com/codehub/techniconrenovations/resources/UserResourceTest.java
@@ -4,194 +4,86 @@ import com.codehub.techniconrenovations.dto.PropertyDto;
 import com.codehub.techniconrenovations.dto.RepairDto;
 import com.codehub.techniconrenovations.dto.RestApiResult;
 import com.codehub.techniconrenovations.dto.UserDto;
+import com.codehub.techniconrenovations.enums.PropertyType;
+import com.codehub.techniconrenovations.enums.RepairType;
+import com.codehub.techniconrenovations.enums.Status;
+import com.codehub.techniconrenovations.model.Property;
+import com.codehub.techniconrenovations.model.PropertyOwner;
+import com.codehub.techniconrenovations.model.PropertyRepair;
 import com.codehub.techniconrenovations.services.PropertyOwnerServices;
+import com.codehub.techniconrenovations.util.UtilFunctions;
 import jakarta.ws.rs.core.Response;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class UserResourceTest {
+    
+    UserResource userResource;
+    PropertyOwnerServices propertyOwnerServices;
+    Property property;
+    PropertyDto propertyDto;
+    PropertyOwner propertyOwner;
+    UserDto userDto;
+    PropertyRepair repair;
+    RepairDto repairDto;
+    List<PropertyDto> properties;
+    List<RepairDto> repairs;
+
+    @Before
+    public void setUp() {
+        userResource = new UserResource();
+        propertyOwnerServices = mock(PropertyOwnerServices.class);
+        userResource.propertyOwnerServices = propertyOwnerServices;
+        userResource.propertyOwnerServices = propertyOwnerServices;
+        propertyOwner = new PropertyOwner(1, "name", "surname", "address", "phoneNumber", "email", "username", "password", "typeOfUser");
+        userDto = new UserDto(propertyOwner);
+        property = new Property("E9", "123 Main St", 2000, PropertyType.APARTMENT_BUILDING, propertyOwner);
+        propertyDto = new PropertyDto(property);
+        repair = new PropertyRepair(propertyOwner, property, RepairType.PAINTING, UtilFunctions.stringToDate("00/00/0000"), "shortDescription", "description", Status.PENDING);
+        repairDto = new RepairDto(repair);
+        properties = new ArrayList<>();
+        properties.add(propertyDto);
+        repairs = new ArrayList<>();
+        repairs.add(repairDto);
+   }
 
     @Test
-    public void testPing() {
-        UserResource userResource = new UserResource();
-        Response response = userResource.ping();
+    public void testRegisterProperty_duplicateEntry() {
+        Response response = userResource.registerProperty(propertyDto);
+        assertEquals(404, response.getStatus());
+    }
+    
+    @Test
+    public void testCorrectPropertyAddress() {    
+        when(userResource.propertyOwnerServices.correctPropertyAddress(1, "E9", "123 Main St")).thenReturn(true);
+        Response response = userResource.correctPropertyAddress(propertyDto);
         assertEquals(200, response.getStatus());
-        assertEquals("pang", response.getEntity());
-    }
-
-    @Test
-    public void testCorrectPropertyAddressWithWrongData() {
-        UserResource userResource = new UserResource();
-        PropertyOwnerServices propertyOwnerServices = mock(PropertyOwnerServices.class);
-        PropertyDto dto = new PropertyDto();
-        userResource.propertyOwnerServices = propertyOwnerServices;
-        when(propertyOwnerServices.correctPropertyAddress(123, "123456", "123 Address")).thenReturn(false);
-
-        Response response = userResource.correctPropertyAddress(dto);
-        assertEquals(404, response.getStatus());
-        assertEquals("Doesn't exist", response.getEntity());
-    }
-
-    @Test
-    public void testCorrectPropertyConstructionYearWithInvalidData() {
-        UserResource userResource = new UserResource();
-        PropertyOwnerServices propertyOwnerServices = mock(PropertyOwnerServices.class);
-        PropertyDto dto = new PropertyDto();
-        userResource.propertyOwnerServices = propertyOwnerServices;
-        when(propertyOwnerServices.correctPropertyConstructionYear(12345, "abc123", 2021)).thenReturn(false);
-
-        Response response = userResource.correctPropertyconstructionYear(dto);
-        assertEquals(404, response.getStatus());
-        assertEquals("Doesn't exist", response.getEntity());
     }
 
     @Test
     public void testGetProperties() {
-        UserResource userResource = new UserResource();
-        PropertyOwnerServices propertyOwnerServices = mock(PropertyOwnerServices.class);
-        userResource.propertyOwnerServices = propertyOwnerServices;
-        List<PropertyDto> properties = Arrays.asList(new PropertyDto());
         when(propertyOwnerServices.getProperties(123)).thenReturn(properties);
-
         RestApiResult result = userResource.getProperties(123);
         assertEquals(200, result.getErrorCode());
-        assertEquals("Successful", result.getDescription());
         assertEquals(properties, result.getData());
     }
 
     @Test
-    public void testGetPropertiesWithEmptyResult() {
-        UserResource userResource = new UserResource();
-        PropertyOwnerServices propertyOwnerServices = mock(PropertyOwnerServices.class);
-        userResource.propertyOwnerServices = propertyOwnerServices;
-        List<PropertyDto> properties = new ArrayList<>();
-        when(propertyOwnerServices.getProperties(123)).thenReturn(properties);
-
-        RestApiResult result = userResource.getProperties(123);
-        assertEquals(404, result.getErrorCode());
-        assertEquals("UnSuccessful", result.getDescription());
-    }
-
-    @Test
-    public void testAcceptOrDeclineRepairWithWrongData() {
-        UserResource userResource = new UserResource();
-        PropertyOwnerServices propertyOwnerServices = mock(PropertyOwnerServices.class);
-        RepairDto dto = new RepairDto();
-        userResource.propertyOwnerServices = propertyOwnerServices;
-        when(propertyOwnerServices.acceptOrDeclineRepair(123, 1, true)).thenReturn(false);
-
-        Response response = userResource.acceptOrDeclineRepair(dto);
-        assertEquals(404, response.getStatus());
-        assertEquals("Doesn't exist", response.getEntity());
+    public void testGetUser() {
+        RestApiResult result = userResource.getUser(123);
+        assertEquals(200, result.getErrorCode());
     }
 
     @Test
     public void testGetRepairStatus() {
-        UserResource userResource = new UserResource();
-        PropertyOwnerServices propertyOwnerServices = mock(PropertyOwnerServices.class);
-        userResource.propertyOwnerServices = propertyOwnerServices;
-        List<RepairDto> repairs = Arrays.asList(new RepairDto());
         when(propertyOwnerServices.getRepairStatus(123)).thenReturn(repairs);
         RestApiResult result = userResource.getRepairStatus(123);
         assertEquals(200, result.getErrorCode());
-        assertEquals("Successful", result.getDescription());
-    }
-
-    @Test
-    public void testGetRepairStatusNoRepairs() {
-        UserResource userResource = new UserResource();
-        PropertyOwnerServices propertyOwnerServices = mock(PropertyOwnerServices.class);
-        userResource.propertyOwnerServices = propertyOwnerServices;
-        when(propertyOwnerServices.getRepairStatus(123)).thenReturn(new ArrayList<>());
-
-        RestApiResult result = userResource.getRepairStatus(123);
-        assertEquals(404, result.getErrorCode());
-        assertEquals("UnSuccessful", result.getDescription());
-    }
-
-    @Test
-    public void testCorrectOwnerUsernameFailure() {
-        UserResource userResource = new UserResource();
-        PropertyOwnerServices propertyOwnerServices = mock(PropertyOwnerServices.class);
-        UserDto dto = new UserDto();
-        userResource.propertyOwnerServices = propertyOwnerServices;
-        when(propertyOwnerServices.correctOwnerUsername(123, "newUsername")).thenReturn(false);
-        Response response = userResource.correctOwnerUsername(dto);
-        assertEquals(404, response.getStatus());
-        assertEquals("Doesn't exist", response.getEntity());
-    }
-
-    @Test
-    public void testCorrectOwnerEmailFailure() {
-        UserResource userResource = new UserResource();
-        PropertyOwnerServices propertyOwnerServices = mock(PropertyOwnerServices.class);
-        UserDto dto = new UserDto();
-        userResource.propertyOwnerServices = propertyOwnerServices;
-        when(propertyOwnerServices.correctOwnerEmail(123, "new@email.com")).thenReturn(false);
-        Response response = userResource.correctOwnerEmail(dto);
-        assertEquals(401, response.getStatus());
-    }
-
-    @Test
-    public void testCorrectOwnerPasswordFailure() {
-        UserResource userResource = new UserResource();
-        PropertyOwnerServices propertyOwnerServices = mock(PropertyOwnerServices.class);
-        UserDto dto = new UserDto();
-        userResource.propertyOwnerServices = propertyOwnerServices;
-        when(propertyOwnerServices.correctOwnerPassword(123, "newPassword")).thenReturn(false);
-
-        Response response = userResource.correctOwnerPassword(dto);
-        assertEquals(404, response.getStatus());
-        assertEquals("Doesn't exist", response.getEntity());
-    }
-
-    @Test
-    public void testSafelyDeleteProperty() {
-        UserResource userResource = new UserResource();
-        PropertyOwnerServices propertyOwnerServices = mock(PropertyOwnerServices.class);
-        PropertyDto dto = new PropertyDto();
-        userResource.propertyOwnerServices = propertyOwnerServices;
-        when(propertyOwnerServices.safelyDeleteProperty(123, "123456")).thenReturn(true);
-        Response response = userResource.safelyDeleteProperty(dto);
-        assertEquals(404, response.getStatus());
-    }
-
-
-    @Test
-    public void testSafelyDeletePropertyRepair() {
-        UserResource userResource = new UserResource();
-        PropertyOwnerServices propertyOwnerServices = mock(PropertyOwnerServices.class);
-        RepairDto dto = new RepairDto();
-        userResource.propertyOwnerServices = propertyOwnerServices;
-        when(propertyOwnerServices.safelyDeletePropertyRepair(123, 1)).thenReturn(true);
-        Response response = userResource.safelyDeletePropertyRepair(dto);
-        assertEquals(404, response.getStatus());
-    }
-
-    @Test
-    public void testSafelyDeletePropertyOwner() {
-        UserResource userResource = new UserResource();
-        PropertyOwnerServices propertyOwnerServices = mock(PropertyOwnerServices.class);
-        UserDto dto = new UserDto();
-        userResource.propertyOwnerServices = propertyOwnerServices;
-        when(propertyOwnerServices.safelyDeletePropertyOwner(123)).thenReturn(true);
-        Response response = userResource.safelyDeletePropertyOwner(dto);
-        assertEquals(404, response.getStatus());
-    }
-
-    @Test
-    public void testSafelyDeletePropertyOwnerWithWrongData() {
-        UserResource userResource = new UserResource();
-        PropertyOwnerServices propertyOwnerServices = mock(PropertyOwnerServices.class);
-        UserDto dto = new UserDto();
-        userResource.propertyOwnerServices = propertyOwnerServices;
-        when(propertyOwnerServices.safelyDeletePropertyOwner(123)).thenReturn(false);
-        Response response = userResource.safelyDeletePropertyOwner(dto);
-        assertEquals(404, response.getStatus());
+        assertEquals(repairs, result.getData());
     }
 }
